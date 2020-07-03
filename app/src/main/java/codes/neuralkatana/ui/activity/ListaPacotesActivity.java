0afef14a753223eb2de49aa466fa246d50d07aa8
neuralrunner <brunohcagnin@gmail.com>
2020-07-03 18:43:13 -0300

    Criação de dados Mockup e Classes especificas. Melhorias na UI e utilização do android Tools para visualização do design.

--- app/src/main/java/codes/neuralkatana/ui/activity/ListaPacotesActivity.java
+++ app/src/main/java/codes/neuralkatana/ui/activity/ListaPacotesActivity.java
@@ -3,14 +3,23 @@ package codes.neuralkatana.ui.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
 
 import codes.neuralkatana.R;
+import codes.neuralkatana.dao.PacoteDAO;
+import codes.neuralkatana.model.Pacote;
+import codes.neuralkatana.ui.adapter.ListaPacotesAdapter;
 
 public class ListaPacotesActivity extends AppCompatActivity {
+
+    private static final PacoteDAO dao = new PacoteDAO();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_pacotes);
+        ListView listaDePacotes = findViewById(R.id.lista_pacotes_listview);
+        listaDePacotes.setAdapter(new ListaPacotesAdapter(dao.lista(),this));
     }
 }
---
+++ app/src/main/java/codes/neuralkatana/ui/adapter/ListaPacotesAdapter.java
@@ -0,0 +1,46 @@
+package codes.neuralkatana.ui.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+import java.util.List;
+
+import codes.neuralkatana.R;
+import codes.neuralkatana.model.Pacote;
+
+public class ListaPacotesAdapter extends BaseAdapter {
+
+    private final List<Pacote> pacotes;
+    private Context context;
+
+    public ListaPacotesAdapter(List<Pacote> pacotes, Context context){
+        this.pacotes = pacotes;
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return pacotes.size();
+    }
+
+    @Override
+    public Pacote getItem(int posicao) {
+        return pacotes.get(posicao);
+    }
+
+    @Override
+    public long getItemId(int posicao) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int posicao, View convertView, ViewGroup parent) {
+        View viewCriada = LayoutInflater
+                .from(context)
+                .inflate(R.layout.item_pacote, parent, false);
+        return viewCriada;
+    }
+}
